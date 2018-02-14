57925c610536faf8f31eb8c16e36ffbb90fc27c9
Nathan Cheung <dunathan@gmail.com>
2018-02-13 23:21:07 -0800

    passing tests for difference-of-squares

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,34 @@
+squares = {}
+sums = {}
+
+
+def get_square(n):
+    if n in squares:
+        return squares[n]
+
+    squares[n] = n * n
+    return squares[n]
+
+
 def square_of_sum(count):
-    pass
+    if count in sums:
+        return sums[count]
+
+    total = 0
+    for i in xrange(count):
+        total += i + 1
+
+    sums[count] = total * total
+    return sums[count]
 
 
 def sum_of_squares(count):
-    pass
+    total = 0
+    for i in xrange(count):
+        total += get_square(i + 1)
+
+    return total
 
 
 def difference(count):
-    pass
+    return square_of_sum(count) - sum_of_squares(count)
